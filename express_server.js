ca62e08c419e3cf26b3ffc8cc3a5cd000e2a264e
luke-berzins <lukeberzins16@gmail.com>
2020-12-11 14:43:42 +0000

    cleaned up commenting style and deconstructed functions

--- express_server.js
+++ express_server.js
@@ -5,6 +5,13 @@ const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 //helper functions
 const helpers = require('./helpers');
+// deconstuct helpers
+const generateRandomString = helpers.generateRandomString;
+const deleteItem = helpers.deleteItem;
+const editItem = helpers.editItem;
+const urlsForUser = helpers.urlsForUser;
+const checkUser = helpers.checkUser;
+const createUser = helpers.createUser;
 
 //SERVER
 const PORT = 8080;
@@ -23,22 +30,24 @@ app.use(cookieSession({
 
 //DATABASES
 const userDatabase = { //structure of database
-  // O1qFUflm: { id: 'O1qFUflm', email: 'lukeberzins16@gmail.com',password: 'chocolate_chip' },
+  // O1qFUflm: { id: 'O1qFUflm', email: 'lukeberzins16@gmail.com', password: 'chocolate_chip' },
   // cwW921dh: { id: 'cwW921dh', email: 'funky-chicken-234@hotmail.com', password: 'pancakes' }
 };
 
 const urlDatabase = { //structure of database
   // b6UTxQ: { longURL: "https://www.clubpenguin.ca", userID: "aJ48lW" },
-  // i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+  // i3BoGr: { longURL: "https://www.clubpenguinPRO.ca", userID: "aJ48lW" }
 };
 
 // APP GETS
 app.get("/", (req, res) => {
   if (req.session["user_id"]) {
-    const urlList = helpers.urlsForUser(req.session["user_id"]["id"], urlDatabase); //filter url database to their links
+    //filter url database to their links
     const templateVars = { user: req.session["user_id"], urls: urlList };
+    const urlList = urlsForUser(req.session["user_id"]["id"], urlDatabase);
     res.render("urls_index", templateVars);
-  } else { //if not logged in
+  } else {
+    //if not logged in
     const templateVars = { user: req.session["user_id"], error1: null, error2: null };
     res.render("login_user", templateVars);
   }
@@ -46,18 +55,21 @@ app.get("/", (req, res) => {
 
 // GET URL PAGES
 app.get("/urls", (req, res) => {
-  if (req.session["user_id"]) { //if user is logged in
-    const urlList = helpers.urlsForUser(req.session["user_id"]["id"], urlDatabase); //filter url database to their links
+  if (req.session["user_id"]) {
+    //if user is logged in filter url database to their link
+    const urlList = urlsForUser(req.session["user_id"]["id"], urlDatabase);
     const templateVars = { user: req.session["user_id"], urls: urlList };
     res.render("urls_index", templateVars);
-  } else { //if not logged in
-    const templateVars = { user: null }; //user value affects urls_index rendering
+  } else {
+    //if not logged in
+    //user value affects urls_index rendering, needs to be defined even if null
+    const templateVars = { user: null };
     res.render("urls_index", templateVars);
   }
 });
 
 app.get("/urls/new", (req, res) => {
-  if (req.session["user_id"]) { //accessible only if logged in
+  if (req.session["user_id"]) {
     const templateVars = { user: req.session["user_id"]};
     res.render("urls_new", templateVars);
   } else {
@@ -67,47 +79,62 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  if (req.session['user_id'] === undefined) { //prevent a user with no cookies from throwing an error for trying to access id property
+  //prevent a user with no cookies from throwing an error for trying to access id property
+  if (req.session['user_id'] === undefined) {
     const templateVars = { user: req.session["user_id"] };
     res.render("404_page", templateVars);
+  //check if the user's id matches the song, if it does give them the page
   } else if (urlDatabase[req.params.id] && urlDatabase[req.params.id]["userID"] === req.session['user_id']['id']) {
     const templateVars = { user: req.session["user_id"], shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
-    res.render("urls_show", templateVars); //check if the user's id matches the song, if it does give them the page
+    res.render("urls_show", templateVars);
   } else {
+  //if it doesnt give 404
     const templateVars = { user: req.session["user_id"] };
-    res.render("404_page", templateVars); //if it doesnt give 404
+    res.render("404_page", templateVars);
   }
 });
 
-app.get("/u/:id", (req, res) => { //will redirect to targeted page
-  const longURL = urlDatabase[req.params.id]["longURL"];
-  res.redirect(longURL);
+app.get("/u/:id", (req, res) => {
+  //if it doesnt exist give 404
+  if (urlDatabase[req.params.id] === undefined) {
+    const templateVars = { user: req.session["user_id"] };
+    res.render("404_page", templateVars);
+  } else {
+  //will redirect to targeted page
+    const longURL = urlDatabase[req.params.id]["longURL"];
+    res.redirect(longURL);
+  }
 });
 
 // POST URL MAKING, EDITING AND DELETING
 
 app.post("/urls", (req, res) => {
-  let shortened = helpers.generateRandomString(6);
-  helpers.editItem(urlDatabase, shortened, req.body.longURL, req.session["user_id"]["id"]); //makes new url in database
+  //makes new url in database
+  let shortened = generateRandomString(6);
+  editItem(urlDatabase, shortened, req.body.longURL, req.session["user_id"]["id"]);
   res.redirect(`urls/${shortened}`);
 });
 
-app.post("/urls/:id/delete", (req, res) => { //delete button
-  if (req.session["user_id"]["id"] === urlDatabase[req.params.id]["userID"]) { //conditional ensures only user can delete their links
-    helpers.deleteItem(urlDatabase, req.params.id);
+app.post("/urls/:id/delete", (req, res) => {
+  //delete button
+  if (req.session["user_id"]["id"] === urlDatabase[req.params.id]["userID"]) {
+  //conditional ensures only user can delete their links
+    deleteItem(urlDatabase, req.params.id);
   }
   res.redirect(`/urls`);
 });
 
-app.post("/urls/:id/edit", (req, res) => { //edit button
-  if (req.session["user_id"]["id"] === urlDatabase[req.params.id]["userID"]) {//conditional ensures only user can edit their links
-    helpers.editItem(urlDatabase, req.params.id, req.body.longURL, req.session["user_id"]["id"]);
+app.post("/urls/:id/edit", (req, res) => {
+  //edit button
+  if (req.session["user_id"]["id"] === urlDatabase[req.params.id]["userID"]) {
+  //conditional ensures only user can edit their links
+    editItem(urlDatabase, req.params.id, req.body.longURL, req.session["user_id"]["id"]);
   }
   res.redirect(`/urls/${req.params.id}`);
 });
 
 // GET LOGIN AND REGISTRATION
-app.get("/login", (req, res) => { //login page
+app.get("/login", (req, res) => {
   if (req.session["user_id"]) {
     res.redirect("urls");
   } else {
@@ -116,7 +143,7 @@ app.get("/login", (req, res) => { //login page
   }
 });
 
-app.get("/register", (req, res) => { //registration page
+app.get("/register", (req, res) => {
   if (req.session["user_id"]) {
     res.redirect("urls");
   } else {
@@ -125,33 +152,27 @@ app.get("/register", (req, res) => { //registration page
   }
 });
 
-
-// DATABASE VIEWER uncomment to see databases from browswer if you are debugging database interactions
-app.get("/urls-data", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/user-data", (req, res) => {
-  res.json(userDatabase);
-});
-
 // APP POSTS
 
 // POST LOGIN AND REGISTRATION
 
 app.post(`/login`, (req, res) => {
-  const value = (helpers.checkUser("email", req.body["email"], userDatabase)); //will return an object of the user if it exists
-  let passwordCheck; //need to set this or else "if (value && passwordCheck)" will fail without else statement intializing ***
+  //will return an object of the user if it exists
+  const value = (checkUser("email", req.body["email"], userDatabase));
+  let passwordCheck;
   if (!value) {
+    //if user doesnt exist send error
     const templateVars = { user: req.session["user_id"], error1: true, error2: null };
-    res.render("login_user", templateVars); //if user doesnt exist send error
-  } else { // *** this is the else statement the above comment is referring to
+    res.render("login_user", templateVars);
+  } else {
     passwordCheck = bcrypt.compareSync(req.body["password"], value["password"]);
   }
-  if (value && passwordCheck) { //if username and password match log in
+  //if username and password match log in
+  if (value && passwordCheck) {
     req.session["user_id"] = value;
     res.redirect(`/urls`);
-  } else if (value && !passwordCheck) { //if user exists and password is incorrect send error
+  } else if (value && !passwordCheck) {
+  //if user exists and password is incorrect send error
     const templateVars = { user: req.session["user_id"], error1: null, error2: true };
     res.render("login_user", templateVars);
   } else {
@@ -161,31 +182,25 @@ app.post(`/login`, (req, res) => {
 
 app.post(`/register`, (req, res) => {
   if (req.body["email"] === '' || req.body["password"] === '') {
+    //if nothing was given send error
     const templateVars = { user: req.session["user_id"], error1: true, error2: null };
-    res.render("register_user", templateVars); //if nothing was given send error
-  } else if (helpers.checkUser("email", req.body["email"], userDatabase)) {
+    res.render("register_user", templateVars);
+  } else if (checkUser("email", req.body["email"], userDatabase)) {
+    //if user exists send error
     const templateVars = { user: req.session["user_id"], error1: null, error2: true };
-    res.render("register_user", templateVars); //if user exists send error
+    res.render("register_user", templateVars);
   } else {
-    const user = helpers.createUser(req.body["email"], bcrypt.hashSync(req.body["password"], 10), userDatabase);
-    req.session['user_id'] = user; //if new user and password then register
+    //if new user and password then register in database
+    const user = createUser(req.body["email"], bcrypt.hashSync(req.body["password"], 10), userDatabase);
+    req.session['user_id'] = user;
     res.redirect(`/urls`);
   }
 });
 
 app.post(`/logout`, (req, res) => {
-  req.session = null; //reset cookies
+  req.session = null;
   res.redirect("/urls");
 });
-
-
-
-// 404 PAGE bug with, newly generated tinies redirect here rather than to their individual page
-
-// app.get('*', function(req, res){
-//   const templateVars = { user: req.session["user_id"]}
-//   res.render("404_page", templateVars);
-// });
 
 
 // SERVER LISTEN
